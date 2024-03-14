5b6e5fe78855b5e3dbda87e827df69bd1a70f8b9
Onkar <112417024+Onkar-Borwar@users.noreply.github.com>
2024-03-14 13:24:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["DevOps Professional","YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Cloud Operation Professional","YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["DevOps Professional","YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Cloud Operation Professional","YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
